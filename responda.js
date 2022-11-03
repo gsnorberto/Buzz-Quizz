4abfb15f0b02c3c6c0c9ed3620d26e05f428c594
gsnorberto <sn_gabriel@outlook.com>
2022-11-02 21:04:06 -0300

    feat: diminui a opacidade das respostas não clicadas, marca de verde a resposta certa, e de vermelho as respostas erradas

--- responda.js
+++ responda.js
@@ -1,18 +1,20 @@
 let todosOsQuizzes = [];
+let respostasCorretas = [];
+let quizzClicado = {}; // Todas as informações do Quizz
 
 // (Provisório) Obter todos os Quizzes
 axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes")
     .then((response) => {
         todosOsQuizzes = response.data;
         console.log(todosOsQuizzes);
-        renderizarQuizzes(14933);
+        renderizarQuizzes(15230);
     }).catch((error) => {
         console.log("Erro ao obter todos os Quizzes");
     });
 
 //Renderizar elementos na tela
 function renderizarQuizzes(id) {
-    let quizzClicado = (todosOsQuizzes.filter((q) => q.id === id))[0];
+    quizzClicado = (todosOsQuizzes.filter((q) => q.id === id))[0];
     console.log(quizzClicado);
 
     //Título do quizz: ok
@@ -32,26 +34,32 @@ function renderizarQuizzes(id) {
         let respostasQuizz = "";
 
         //Posicionar respostas aleatoriamente
-        let posicoesPerguntas = [];
+        let posicoesRespostas = [];
         for(let k = 0; k < quizzClicado.questions[i].answers.length; k++){
             //EX: Se a qnt de respostas for 3, então cria array [0, 1, 2]
-            posicoesPerguntas.push(k);
+            posicoesRespostas.push(k);
         }
-        posicoesPerguntas.sort(comparador); // embaralha array
+        posicoesRespostas.sort(comparador); // embaralha array
 
         //Adicionar respostas em posições aleatórias
         for(let j = 0; j < quizzClicado.questions[i].answers.length; j++){
+            //Resposta renderizada em posição de forma aleatória
+            let res = quizzClicado.questions[i].answers[posicoesRespostas[j]];
+
             respostasQuizz += `
-                <div class="resposta-quizz">
+                <div id="${i}${posicoesRespostas[j]}" onclick="verificarResposta(${posicoesRespostas[j]}, ${i})" class="resposta-quizz">
                     <img class="resposta-img"
-                        src=${quizzClicado.questions[i].answers[posicoesPerguntas[j]].image}
+                        src=${res.image}
                         alt="">
-                    <div class="resposta-desc">${quizzClicado.questions[i].answers[posicoesPerguntas[j]].text}</div>
+                    <div class="resposta-desc">${res.text}</div>
                 </div>
             `
 
             //Pegar a resposta correta
-            // pendente...
+            let respostaVerdadeira = res.isCorrectAnswer;
+            if(respostaVerdadeira){
+                respostasCorretas.push(posicoesRespostas[j]);
+            }
         }
 
         //PERGUNTA QUIZZ - RENDERIZAR NA TELA
@@ -71,4 +79,31 @@ function renderizarQuizzes(id) {
     
     //Tirar oculto da tag area quizz
     // pendente.... 
+}
+
+function verificarResposta(resClicada, numDaQuestao){
+    //Resposta verdadeira
+    if(respostasCorretas[numDaQuestao] == resClicada){
+        
+        console.log("Acertou")
+    } else { //Resposta verdadeira
+        console.log("Errou")
+    }
+
+    //Marcar respostas certas e erradas
+    for(let i = 0; i < quizzClicado.questions[numDaQuestao].answers.length; i++){
+        //resposta certa
+        if(respostasCorretas[numDaQuestao] == i){
+            document.getElementById(`${numDaQuestao}${i}`).style.color = "#009C22";
+        } else { // resposta errada
+            document.getElementById(`${numDaQuestao}${i}`).style.color = "#FF4B4B";
+        }
+
+        //Diminui a opacidade dos itens não clicados
+        if(resClicada !== i){
+            document.getElementById(`${numDaQuestao}${i}`).style.opacity = "0.3";
+        }
+
+        document.getElementById(`${numDaQuestao}${i}`).removeAttribute("onclick");
+    }
 }
