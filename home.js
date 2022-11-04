b3786898a81a06244786fe853f38c7698a48eec4
RodrigoD1a5 <rodrigobastos.diass@gmail.com>
2022-11-04 01:11:04 -0300

    Versão com quizzes do servidor reinderizados

--- home.js
+++ home.js
@@ -1,6 +1,9 @@
 //---------(Provisorio) Dinâmica das duas telas dos Quizzes do Usuário-------- //
 const quizzesVazio= document.querySelector(".quizzes-usuario-vazio");
 const quizzesUsuario=document.querySelector(".quizzes-usuario");
+const quizzesGerais = document.querySelector(".todos-os-quizzes");
+
+//------------Função que vai reinderizar quizzes do usuario, caso haja----------------//
 function reconfigurarHomeUsuario(){
     
     quizzesVazio.classList.toggle("escondido");
@@ -8,7 +11,30 @@ function reconfigurarHomeUsuario(){
 }
 
 //------------(Provisorio) Redirecionar para o quizz em questão --------------//
-const body= document.querySelector("body");
+
 function iniciarQuizz(){
+    const body= document.querySelector("body");
     body.classList.add("escondido");
 }
+
+//-------------Função que reinderiza Todos os quizzes do servidor -----------//
+importarQuizzes();
+reinderizarTodosQuizzes();
+function reinderizarTodosQuizzes(resposta){
+    for(let i=0; i<=50;i++){
+        quizzesGerais.innerHTML+=`
+        <div class="quizzes-gerais" onclick="iniciarQuizz()"> 
+            <img src="${resposta.data[i].image}"/>
+            <div class="efeito-imagem"></div>
+            <div class="titulo-quiz">${resposta.data[i].title}</div>
+        </div>
+        `
+    }
+}
+//--------------Função que busca Todos os quizzes do servidor --------------//
+
+function importarQuizzes(){
+    let promise = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
+    promise.then(reinderizarTodosQuizzes);
+}
+
