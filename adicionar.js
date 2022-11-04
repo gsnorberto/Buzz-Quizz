584c305ab13b8b5be912842ed6ba8cea6c058bf8
aabenjamim <alanebenjamimdossantos@gmail.com>
2022-11-03 21:03:24 -0300

    versão com a criação de perguntas

--- adicionar.js
+++ adicionar.js
@@ -1,5 +1,26 @@
 //Validação da tela 3.1 ao apertar no botão de seguir
 
+const infosBasicas = document.querySelector('.infos-basicas')
+const criarPerguntas = document.querySelector('.criar-perguntas')
+const criarNiveis = document.querySelector('.criar-niveis')
+
+
+//verifica se a string é um url
+function verificaUrl(termo) {
+    try {
+     let url = new URL(termo)
+     return true
+    } catch(err) {
+       return false
+    }
+ }
+
+ let numPerguntas
+ let numNiveis
+ let titulo
+ let url
+
+
 function validarInformacoes(){
 
     let tituloQuizz = (document.querySelector(".titulo-quizz")).value
@@ -7,15 +28,126 @@ function validarInformacoes(){
     let qntPerguntas = (document.querySelector(".qntd-perguntas")).value
     let qntNiveis = (document.querySelector(".qntd-niveis")).value
 
-    let urlPadrao = new RegExp("^((http(s?):\/\/(www.)?[a-z]+.com\/)|(magnet:\?xt=urn:btih:))")
-    
+    numPerguntas = qntPerguntas
+    numNiveis = qntNiveis
+    titulo = tituloQuizz
+    url = urlQuizz
+
     if (tituloQuizz.length < 20 || tituloQuizz.length > 65){
-        return alert("Preencha os dados corretamente")
-    } else if(urlPadrao.test(urlQuizz) === false){
-        return alert("Preencha os dados corretamente")
+        console.log('que')
+        return alert("Preencha o título corretamente")
     } else if (qntPerguntas < 3){
-        return alert("Preencha os dados corretamente")
+        return alert("Preencha o número de perguntas corretamente")
     } else if (qntNiveis < 2) {
-        return alert("Preencha os dados corretamente")
+        return alert("Preencha o número de níveis corretamente")
+    } else if (urlQuizz !== '') {
+        if(verificaUrl(urlQuizz) === true){
+            infosBasicas.classList.add('escondido')
+            criarPerguntas.classList.remove('escondido')
+            inserirPerguntas()
+        } else{
+            alert("Preencha o url corretamente")
+        }
+    }
+}
+
+//insere o número de Perguntas escolhido pelo usuário na 1ª tela
+function inserirPerguntas(){
+    const espaco = document.querySelector('.espaco-perguntas')
+    for(let i=0; numPerguntas>i; i++){
+        espaco.innerHTML += `
+        <div class="caixa-pergunta n${i+1}" onclick="editarPergunta(this)">
+        <p>Pergunta ${i+1}</p>
+        <button><img src="./Vector.svg"></button>
+    </div>
+    
+    <form class="n${i+1} escondido">
+        <div class="caixa 1">
+            <p>Pergunta ${i+1}</p>
+            <input type="text" class="texto-pergunta" placeholder="Texto da pergunta">
+            <input type="text" class="cor-fundo" placeholder="Cor de fundo da pergunta">
+        </div>
+        <div class="caixa 2">
+            <p>Resposta correta</p>
+            <input type="text" class="resposta-certa" placeholder="Resposta correta">
+            <input type="text" class="url-imagem" placeholder="URL da imagem">
+        </div>
+        <div class="caixa 3">
+            <p>Respostas incorretas</p>
+            <input type="text" class="resposta-errada 1" placeholder="Resposta incorreta 1">
+            <input type="text" class="url-imagem" placeholder="URL da imagem 1">
+        </div>
+        <div class="caixa 4">
+            <input type="text" class="resposta-errada 2" placeholder="Resposta incorreta 2">
+            <input type="text" class="url-imagem" placeholder="URL da imagem 2">
+        </div>
+        <div class="caixa 5">
+            <input type="text" class="resposta-errada 3" placeholder="Resposta incorreta 3">
+            <input type="text" class="url-imagem" placeholder="URL da imagem 3">
+        </div>
+    </form>
+        `
+    }
+}
+
+//função para abrir o formulário de perguntas ao clicar no ícone de editar
+function editarPergunta(item){
+    const listaClasses = item.classList
+    let classe = listaClasses[1]
+    
+    const formulario = document.querySelector(`form.${classe}`)
+
+    formulario.classList.remove('escondido')
+    item.classList.add('escondido')
+}
+
+// função para validar o formulário de perguntas criadas
+function validarPerguntas(){
+    
+    const form= []
+    for(let i=0; numPerguntas>i; i++){
+        let formulario = document.querySelector(`form.n${i+1}`)
+        form.push(formulario)
+    }
+
+    const padrao = new RegExp("^#([A-Fa-f0-9]{6})$");
+
+    for(let i=0; form.length>i; i++){
+        if((form[i][0].value).length < 20){
+            console.log('titulo invalido')
+            alert('Preencha a pergunta corretamente')
+        } else if (padrao.test(form[i][1].value) === false){
+            console.log('cor invalida')
+            alert('Preencha a cor corretamente')
+        } else if((form[i][2].value) === '' || (form[i][3].value) === ''){
+            console.log("ta vazio a resposta correta")
+            alert("Preencha a resposta corretamente")
+        } else if((form[i][4].value) === '' && (form[i][6].value) === '' && (form[i][8].value) === ''){
+            console.log('ta vazia todas as respostas erradas')
+            alert('Preencha alguma das respostas erradas pelo menos')
+        } else if (form[i][3] !== ''){
+            if(verificaUrl(form[i][3].value) === false){
+                console.log('eita')
+                alert('Preencha a url corretamente')
+            }
+        } else if (form[i][5] !== ''){
+            if(verificaUrl(form[i][5].value) === false){
+                console.log('eita')
+                alert('Preencha a url corretamente')
+            }
+        } else if (form[i][7] !== ''){ 
+            if(verificaUrl(form[i][7].value) === false){
+                console.log('eita')
+                alert('Preencha a url corretamente')
+            }
+        } else if (form[i][9] !== ''){ 
+            if(verificaUrl(form[i][9].value) === false){
+                console.log('eita')
+                alert('Preencha a url corretamente')
+            }
+        } else{
+            criarPerguntas.classList.add('escondido')
+            criarNiveis.classList.remove('escondido')
+        }
     }
 }
